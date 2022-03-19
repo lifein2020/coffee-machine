dc994527d4031d5c803ec9d347b6b68a5737c07b
Svetlana Zh <lady_zhivtsova@gmail.com>
2022-03-19 14:38:18 +0300

    translate

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-# coffee-machine
-Кофе-машина группы 380
+# Coffee-machine
 
-Стэк HTML CSS Bootstrap JS
+### An interactive game that simulates a coffee machine.
+
+##GitHub Pages##
+## Functionality: ##
+* selection of coffee by pressing the corresponding icon
+* acceptance of banknotes, in order to replenish the balance, it is necessary to hold the banknote and move it to the bill acceptor area
+* balance sheet calculation
+* issuance of change, the ability to pick it up by 1 coin or all at once
+* coffee making process
+
+## Technology: ##
+* HTML
+* CSS
+* Bootstrap
+*Java Script
+
+## Plans for finalizing the project: ##
+improve styling, improve style, apply BEM technology, make HTML tags suitable for SEO
--- coffee.js
+++ coffee.js
@@ -1,10 +1,11 @@
 "use strict";
-let state = "waiting"; //глобальная переменная
+//global variables
+let state = "waiting"; 
 
 let progressBar = document.querySelector(".progress-bar");
-let balanceInput = document.querySelector("input[placeholder='Баланс']");
-let cupImg = document.querySelector(".coffee-cup img"); //глобальная переменная, ищем кружку
-cupImg.onclick = takeCoffee; //  вешаем на кружку событие - функцию нажатия на кофе, takeCoffee - тело функции, саму функцию опишем ниже
+let balanceInput = document.querySelector("input[placeholder='Balance sheet']");
+let cupImg = document.querySelector(".coffee-cup img"); //find cup
+cupImg.onclick = takeCoffee; //  hang an event on a cup - the function of clicking on coffee
 
 function buyCoffee(name, price, element) {
   if (state != "waiting") {
@@ -12,18 +13,20 @@ function buyCoffee(name, price, element) {
   }
   
   if (+balanceInput.value < price) {
-    changeDisplayText("Недостаточно средств");
-    balanceInput.style.background = "red" // обращаемся из JS к CSS
+    changeDisplayText("Insufficient funds");
+    balanceInput.style.background = "red" 
   } else {
     balanceInput.value -= price;
     balanceInput.style.border = "";
+    balanceInput.style.background = ""; // the input changes color to the original one if the balance is greater than the price
     state = "cooking";
     cookCoffee(name, element);
   }
+
 }
 
 function cookCoffee(name, buttonElement) {
-  changeDisplayText("Ваш " + name + " готовится");
+  changeDisplayText("Your " + name + " is being prepared");
   let buttonImg = buttonElement.querySelector("img");
   let cupSrc = buttonImg.getAttribute('src');
   
@@ -34,10 +37,10 @@ function cookCoffee(name, buttonElement) {
   let interval = setInterval(function () {
     i++;
     progressBar.style.width = i + "%";
-    cupImg.style.opacity = i + "%"; // кружка появляется при заказе
+    cupImg.style.opacity = i + "%"; // cup appears when ordering
     if (i==110) {
       clearInterval(interval);
-      changeDisplayText("Ваш " + name + " готов!");
+      changeDisplayText("Your " + name + " is ready!");
       cupImg.style.cursor = "pointer";
       state = "ready";
     }
@@ -50,59 +53,56 @@ function takeCoffee() {
   }
   state = "waiting";
   cupImg.style.opacity = 0;
-  cupImg.style.cursor = ""; // "" к прежнему значению
+  cupImg.style.cursor = ""; // "" means to the previous value
   cupImg.classList.add("d-none");
-  changeDisplayText("Выберите кофе");
+  changeDisplayText("Select coffee");
   progressBar.style.width = 0;
 } 
 
 function changeDisplayText(text) {
   let displayText = document.querySelector('.display-text');
-  displayText.innerHTML = text; // обращаемся к внутреннему содержимому переменной displayText и записывает туда параметр text
+  displayText.innerHTML = text; // we access the internal content of the displayText variable and write the text parameter there
 }
 
-//----------Купюры---------
+// ---------- Banknotes ---------
 
 let bills = document.querySelectorAll('.bills img');
 for (let i = 0; i < bills.length; i++) {
-  bills[i].onmousedown = takeMoney; // повесили событие сразу на все купюры
-  /* bills[i].onmousedown = function (event) {
-    takeMoney(event);
-  } второй способ вешания события */
+  bills[i].onmousedown = takeMoney; // hung the event on all banknotes at once
 }
 
 function takeMoney(event) {
-  // alert("Вы нажали на купюру");
   //console.log(event)
-  event.preventDefault(); // вызываем метод preventDefault объекта event. Функция preventDefault () - прерывание действия по умолчанию.
-  //console.log(this) либо
-  let bill = event.target;//цель события - возвращает элемент на который мы нажали
+  event.preventDefault(); // call the preventDefault method of the event object. The preventDefault() function is to interrupt the default action.
+  //console.log(this) or
+  let bill = event.target; // event target - returns the element we clicked on
   
-  bill.style.position = "absolute"; //чтобы двигать по всей странице, absolute вытягивает элемент из DOM-дерева, следовательно их позиции сбрасываются и они накладываются друг на друга при нажатии
+  bill.style.position = "absolute"; // to move across the page, absolute pulls the element out of the DOM tree, so their positions are reset and they overlap when clicked
   bill.style.transform = "rotate(90deg)";
   bill.style.margin = 0;
   
-    let billCoords = bill.getBoundingClientRect(); //вернет размер элемента(купюры) в зависимости от величины экрана
-    let billWidth = billCoords.width;
-    let billHeight = billCoords.height;
-    // console.log(event.clientX, event.clientY); //clientX clientY отображают положение курсора при нажатии на мышь по оси X,Y на экране без привязки полосам прокрутки
+  let billCoords = bill.getBoundingClientRect(); // will return the size of the element (banknote) depending on the size of the screen
+  let billWidth = billCoords.width;
+  let billHeight = billCoords.height;
+  // console.log(event.clientX, event.clientY); // display the position of the cursor when the mouse is pressed along the X, Y axis on the screen without binding to the scroll bars
     
-    bill.style.top = event.clientY - billWidth/2 + "px";// при нажатии на купюру её центр совпадает с курсором
-    bill.style.left = event.clientX - billHeight/2 + "px";// когда элемент переворачивается на 90гр.( rotate(90deg) ) оси x y меняются местами
+  bill.style.top = event.clientY - billWidth/2 + "px";// when you click on a banknote, its center coincides with the cursor
+  bill.style.left = event.clientX - billHeight/2 + "px";// when the element is rotated 90deg.( rotate(90deg) ) the x and y axes are swapped
     
-    window.onmousemove = function(event) { // отловили передвижение мыши по всему экрану, чтобы знать где в текущий момент мышь и подгонять предыдущие значения под текущее положение мыши
-      bill.style.top = event.clientY - billWidth/2 + "px";
-      bill.style.left = event.clientX - billHeight/2 + "px";
+  window.onmousemove = function(event) { // caught mouse movement across the entire screen in order to know where the mouse is at the current moment and adjust the previous values ​​to the current mouse position
+    bill.style.top = event.clientY - billWidth/2 + "px";
+    bill.style.left = event.clientX - billHeight/2 + "px";
+  }
+    
+  bill.onmouseup = function () { 
+    window.onmousemove = null; // so that the banknote comes off the mouse when moving
+    if ( inAtm(bill) ) { // if the bill got into atm
+      let billCost = +bill.getAttribute('cost');
+      balanceInput.value = +balanceInput.value + billCost;
+      bill.remove();
+      balanceInput.style.background = ""; // the input changes color to the original one when a bill enters the receiver
     }
-    
-    bill.onmouseup = function () { // отжатие мыши
-      window.onmousemove = null; // чтобы купюра отлипла от мыши при движении
-      if ( inAtm(bill) ) { // если купюра попала в atm
-        let billCost = +bill.getAttribute('cost');
-        balanceInput.value = +balanceInput.value + billCost;
-        bill.remove();
-      }
-    }
+  }
     
 }
 
@@ -115,12 +115,12 @@ function inAtm(bill) {
   let billLeftTopCorner = {"x" : billCoords.x, "y" : billCoords.y};
   let billRightTopCorner = {"x" : billCoords.x + billCoords.width, "y" : billCoords.y};
   
-  // определяем область ATM, куда должна "вставляться" купюра. Получаем значения по X, Y.
+  // we define the ATM area where the bill should be "inserted". We get values for X, Y.
   let atmLeftTopCorner = {"x" : atmCoords.x, "y" : atmCoords.y};
   let atmRightTopCorner = {"x" : atmCoords.x + atmCoords.width, "y" : atmCoords.y};
   let atmLeftBottomCorner = {"x" : atmCoords.x, "y" : atmCoords.y + atmCoords.height/3 };
   
-  // Сравниваем значения по X купюры и ATM
+  // Compare values for X bills and ATM
   if (billLeftTopCorner.x > atmLeftTopCorner.x
       && billRightTopCorner.x < atmRightTopCorner.x
       && billLeftTopCorner.y > atmLeftTopCorner.y
@@ -133,49 +133,94 @@ function inAtm(bill) {
   
 }
 
-// Как отключить стандартное браузерное событие(призрак купюры), которое висит onmousedown. Любое событие (onmousedown и пр.) автоматически передает в функцию первым параметром event-событие. Чтобы его отловить, укажем в скобках. Т.е. любое событие содержит в себе по умолчанию event.
+// -------- Change --------
 
-// --------Сдача--------
+let changeButton = document.querySelector(".change-btn");
 
-let changeButton = document.querySelector('.change-btn');
-/*changeButton.onclick = function() {
-  takeChange();
-} */
-changeButton.onclick = takeChange;
-
-function takeChange() {
-  tossCoin("10");
+changeButton.onclick = function () {
+  let changeBox = document.querySelector(".change-box");
+  let coins = changeBox.querySelectorAll("img");
+  if (coins.length == 0) {
+    if(balanceInput.value == 0) {
+      return;
+    }
+    changeButton.innerHTML = "Get change";
+    takeChange();
+  } else{
+    // Take all coins
+    changeButton.innerHTML = "Your change";
+    for (let i = 0; i < coins.length; i++) {
+      coins[i].remove();
+    }
+  }
 }
 
-function tossCoin(cost) { //вываливает монетки в div с классом .change-box
+function takeChange() {
+  if(balanceInput.value == 0) {
+    return;
+  }
+  if (balanceInput.value >=10) {
+    balanceInput.value -= 10;
+    tossCoin("10");
+    takeChange();
+  }else if (balanceInput.value >=5) {
+    balanceInput.value -= 5;
+    tossCoin("5");
+    takeChange();
+  }else if (balanceInput.value >=2) {
+    balanceInput.value -= 2;
+    tossCoin("2");
+    takeChange();
+  }else {
+    balanceInput.value -= 1;
+    tossCoin("1");
+    takeChange();
+  }
+}
+
+function tossCoin(cost) { // dumps coins into a div with class .change-box
+  let imgSrc  = ""; // allows you to see what is supposed to put in a variable
+  switch (cost) { 
+    case "10":
+      imgSrc = "img/10rub.png";
+      break;
+    case "5":
+      imgSrc = "img/5rub.png";
+      break;
+    case "2":
+      imgSrc = "img/2rub.png";
+      break;
+    case "1":
+      imgSrc = "img/1rub.png";
+      break;
+  }
   let changeBox = document.querySelector(".change-box"); 
   changeBox.style.position = "relative";
   
   let changeBoxCoords = changeBox.getBoundingClientRect();
   let randomWidth = getRandomInt(0, changeBoxCoords.width - 50);
-  let randomHeight = getRandomInt(0, changeBoxCoords.height - 50);// 50 ширина картинки, чтобы она попадала в ширину картинки
+  let randomHeight = getRandomInt(0, changeBoxCoords.height - 50);// 50 the width of the image so that it fits within the width of the image
   console.log(randomWidth, randomHeight);
   
-  let coin = document.createElement("img"); //создает элемент тэга(img), который вписываем в параметры
-  coin.setAttribute('src', 'img/10rub.png');
+  let coin = document.createElement("img"); // creates an element of the tag (img), which we enter into the parameters
+  coin.setAttribute('src', imgSrc);
   coin.style.width = "50px";
   coin.style.height = "50px";
+  coin.style.cursor = "pointer";
   coin.style.position = "absolute";
   coin.style.top = randomHeight + "px";
   coin.style.left = randomWidth + "px";
   
-  changeBox.append(coin); // append - добавляем элемент coin в конец предыдущего(родительского) элемента changeBox. Из учебника Документ -> 1.7 Изменение документа
-  //changeBox.prepend(coin);//Добавлям в начало
-  //changeBox.before(coin); // перед элементом
-  //changeBox.after(coin); // после элемента
-  //changeBox.replaceWith(coin); //заменяет элемент
+  changeBox.append(coin); // append - add a coin element to the end of the previous (parent) changeBox element.
+  coin.onclick = function() { // Check how many coins are left in the container
+    coin.remove();
+  }
   
 }
 
-//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random :
 function getRandomInt(min, max) { 
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+  return Math.floor(Math.random() * (max - min)) + min; // Maximum not included, minimum included
 }
 
